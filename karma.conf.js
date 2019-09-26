f172c38a9c5500272348b71fe847cb526fc6e90e
Anjireddy.kata <anjireddy.kata@gmail.com>
2019-09-26 08:45:12 +0530

    added chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-     // require('karma-chrome-launcher'),
+      require('karma-chrome-launcher'),
       require( 'karma-phantomjs-launcher' ),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -26,18 +26,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
-    // customLaunchers: {
-    //   'ChromeHeadless': {
-    //     base: 'Chrome',
-    //     flags: [
-    //       '--no-sandbox',
-    //       '--headless',
-    //       '--disable-gpu',
-    //       '--remote-debugging-port=9222'
-    //     ]
-    //   }
-    // },
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
