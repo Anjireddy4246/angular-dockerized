2175375de4b64139f6f97f36a76c2bf4d73d3430
Anjireddy.kata <anjireddy.kata@gmail.com>
2019-09-27 18:59:18 +0530

    added subtitle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'angular-fireball';
+  subTitle = 'awesome angular';
   ngOnInit(): void {
     if (this.title == 'angular-fireball') {
       console.log('Angular App is up and running');
