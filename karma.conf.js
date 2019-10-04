f23cb8d6106c7600708395c4affa891b75043ba1
Anjireddy.kata <anjireddy.kata@gmail.com>
2019-10-04 14:45:29 +0530

    added reports

--- karma.conf.js
+++ karma.conf.js
@@ -13,22 +13,28 @@ module.exports = function (config) {
       require( 'karma-phantomjs-launcher' ),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-sonarqube-unit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/angular-dockerized'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary','cobertura'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml','sonarqubeUnit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
+    sonarQubeUnitReporter: {
+      sonarQubeVersion: 'LATEST',
+      outputFile: 'reports/ut_report.xml',
+      useBrowserName: false
+    },
     customLaunchers: {
       'ChromeHeadless': {
         base: 'Chrome',
